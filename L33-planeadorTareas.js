557286cac89d5e04d9aefc05ccfdb44485e67129
andresropk <afrokap@gmail.com>
2023-03-16 23:41:37 -0500

    chore: L33 test rig

--- L33-planeadorTareas.js
+++ L33-planeadorTareas.js
@@ -1,3 +1,6 @@
+let log = console.log
+log('\n\n\n-----------------------\n--------------------------\n\n\n')
+
 function createTaskPlanner() {
   let tasks = [];
   return {
@@ -17,17 +20,17 @@ function createTaskPlanner() {
       return tasks.filter((t) => t.completed == true);
     },
     markTaskAsCompleted(val) {
-      tasks.find((t) => t.id == val || t.name == val).completed == true;
+      tasks.find((t) => t.id == val || t.name == val).completed = true;
     },
     getSortedTasksByPriority() {
       return tasks.sort((a, b) => {a.priority - b.priority});
     },
     filterTasksByTag(val){
-      return tasks.filter(t=>t.tags.some(tg=>tg.includes(val)));
+      return tasks.filter(t=>t.tags.some(tg=>tg == val));
     },
     updateTask(taskId,updates){
         let task = tasks.find(t=>t.id == taskId)
-        for(let prop of updates){
+        for(let prop in updates){
             task[prop] = updates.prop
         }
     }
@@ -52,38 +55,47 @@ planner.addTask({
   tags: ["personal"],
 });
 
+planner.addTask({
+    id: 3,
+    name: "Task 3",
+    priority: 2,
+    tags: ["tag1", "tag2"],
+});
+
+planner.addTask({
+    id: 4,
+    name: "Task 4",
+    priority: 1,
+    tags: ["tag4a", "tag4b","tag4c"],
+});
+
+planner.addTask({
+    id: 5,
+    name: "Task 5",
+    priority: 2,
+    tags: ["tag5a"],
+});
+
+planner.addTask({
+    id: 6,
+    name: "Task 6",
+    priority: 1,
+    tags: ["tag6a","tag6b"],
+});
+
+
+planner.removeTask(5);
+let ans1 = planner.getPendingTasks();
+log('1\n',ans1)
 planner.markTaskAsCompleted("Llamar a Juan");
+let ans2 = planner.getCompletedTasks();
+let ans3 = planner.getPendingTasks();
+log('3\n',ans3)
+let ans4 = planner.getSortedTasksByPriority();
+log('4\n',ans4)
+let ans5 = planner.filterTasksByTag("shopping");
+planner.updateTask(6,{name:'6 change',tags:['tag6a','tag6b','tag6c']})
+let ans6 = planner.getTasks()
+log('6\n',ans6)
 
-console.log(planner.getCompletedTasks());
-// [{
-//     id: 2,
-//     name: "Llamar a Juan",
-//     completed: true,
-//     priority: 3,
-//     tags: ["personal"]
-// }]
 
-planner = createTaskPlanner();
-
-planner.addTask({
-  id: 1,
-  name: "Comprar leche",
-  priority: 1,
-  tags: ["shopping", "home"],
-});
-
-planner.addTask({
-  id: 2,
-  name: "Llamar a Juan",
-  priority: 3,
-  tags: ["personal"],
-});
-
-console.log(planner.filterTasksByTag("shopping"));
-// [{
-//     id: 1,
-//     name: "Comprar leche",
-//     completed: false,
-//     priority: 3,
-//     tags: ["shopping", "home"]
-// }]
